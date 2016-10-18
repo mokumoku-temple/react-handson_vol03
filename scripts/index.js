8cad7542eda10bf9f32eccbd6b9577341bd97416
Keita Moromizato <keita_moromizato@relationsgroup.co.jp>
2016-10-18 19:30:22 +0900

    add material-ui

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 
-render(
-  <div>
-    <h1>Hello</h1>
-  </div>,
-  document.getElementById('app')
-);
+import './index.css';
+
+injectTapEventPlugin();
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showMenu: false,
+    };
+  }
+
+  onMenuStateChange(showMenu) {
+    this.setState({ showMenu });
+  }
+
+  render() {
+    return (
+      <div>
+        <MuiThemeProvider>
+          <div>
+            <AppBar
+              title="React HandsOn vol.03"
+              onLeftIconButtonTouchTap={() => this.onMenuStateChange(true)}
+            />
+            <h1>Hello</h1>
+
+            <Drawer
+              docked={false}
+              open={this.state.showMenu}
+              width={200}
+              onRequestChange={show => this.onMenuStateChange(show)}
+            >
+              <MenuItem>Home</MenuItem>
+              <MenuItem>List</MenuItem>
+            </Drawer>
+          </div>
+        </MuiThemeProvider>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        loader: 'css-loader!stylus-loader',
+        loader: 'style-loader!css-loader',
       },
       {
         test: /\.html$/,
