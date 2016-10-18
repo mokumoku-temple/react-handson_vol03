17e53e6d8672be6b43ddd98812ec090d69048921
Keita Moromizato <keita_moromizato@relationsgroup.co.jp>
2016-10-18 17:53:34 +0900

    Initial Commit

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+render(
+  <div>
+    <h1>Hello</h1>
+  </div>,
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  entry: {
+    js: './scripts/index.js',
+    html: './scripts/index.html',
+  },
+  output: {
+    path: `${__dirname}/out`,
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: 'out',
+    port: 3000,
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react'],
+          plugins: [],
+        },
+      },
+      {
+        test: /\.css$/,
+        loader: 'css-loader!stylus-loader',
+      },
+      {
+        test: /\.html$/,
+        loader: 'file?name=[name].[ext]',
+      },
+    ],
+  },
+  resolve: {
+    modulesDirectories: ['node_modules', './scripts'],
+  },
+};
